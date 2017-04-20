03574432f94f7fdd4d76116ac21ce2666ba03601
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-04-20 16:58:49 -0700

    "another port change"

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var port = process.env.port || 3000;
 
 app.listen(port);
 
-console.log('Server now listening on port ' + port);
+console.log('Server now listening on port ' + port + process.env.port);
